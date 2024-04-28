aee65304405b6db9916ad5e829b7568c44494e6d
Stephen Jones <stephenj99642@gmail.com>
2024-04-28 21:32:42 +0100

    Proud of my integer solution

--- 01-AnyInteger/solver.py
+++ 01-AnyInteger/solver.py
@@ -3,7 +3,7 @@
 
 def solve_it(input_data):
     # return a positive integer, as a string
-    return '0'
+    return '3593628592450023578329877836873620857562053265982023656902035'
 
 if __name__ == '__main__':
     print('This script submits the integer: %s\n' % solve_it(''))
